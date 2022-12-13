c697a9316c4549aff5f30bf1445446a77ded4e96
_ID_ <106934111+Oogway06@users.noreply.github.com>
2022-12-13 11:51:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,20 @@
-/*Inicio*\
+global.prefix = '-'
+global.botName = 'DobbyBot-MD' 
+global.fake = botName + ' ∆IDBQ∆'
+global.groupSupport = 'https://chat.whatsapp.com/KyiLD5Oj9HR7mcJcsdCfCF' 
+
+global.staff = [
+        '595983315802' // yo2
+]
+global.owner = [
+        '595984664076' // Yo2
+]
+global.mess = {
+       wait: 'Por favor espere...',
+       only: {
+               admins: 'Necesitas ser admin para usar este comando', 
+               botadmin: botName + 'necesita ser admin', 
+               group: 'Comando solo para grupos', 
+               vip: 'Comando exclusivo para usuarios *🪄Vip🪄*' 
+         }
+  }
